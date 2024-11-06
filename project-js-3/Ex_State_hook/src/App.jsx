617e98dd29fcd67f93d5139d66bd3bc2ba2d92b0
Karen <karen.yumi20@gmail.com>
2024-11-06 16:06:38 -0300

    Explicação do código

--- project-js-3/Ex_State_hook/src/App.jsx
+++ project-js-3/Ex_State_hook/src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+
 export default function App() {
-    const [price, setPrice] = React.useState("$100");
+    const [getPrice, setPrice] = React.useState("$100");
     function handleSetDiscount(){
         setPrice("$75");
     }
     return (
         <div>
-            <p data-testid="price">{price}</p>
+            <p data-testid="price">{getPrice}</p> 
             <button onClick={handleSetDiscount}>Apply Discount</button>
         </div>
     );
 }
+
+//primeiro ele pega com getPrice o valor 100
+// e ao clicar no botão ele redireciona para a função handleSetDiscount
+// que salva o valor 75 com setPrice
+// e depois ele pega o valor salvo com o getPrice
