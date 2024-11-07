1df618369ca02efc05af46c7d2b892f8aabeaec8
Karen <karen.yumi20@gmail.com>
2024-11-07 15:16:43 -0300

    Separei os componentes para caso um seja recaregado, o outro se mantenha

---
+++ project-jsx-2/src/components/CoreConcepts.jsx
@@ -0,0 +1,25 @@
+import CoreConcept from "./CoreConcept.jsx";
+import { CORE_CONCEPTS } from "../data.js";
+
+export default function CoreConcepts() {
+    return (
+        <section id="core-concepts">
+          <h2> Core Concepts</h2>
+          <ul>
+            {/* <CoreConcept 
+            title="Components" 
+            description='The core UI building block - compose the user interface by combining multiple components.'
+            image ={componentsImg}
+            />
+            <CoreConcept
+            title={CORE_CONCEPTS[1].title}                                      repetição, por isso usamos o map abaixo
+            description={CORE_CONCEPTS[1].description} 
+            image ={CORE_CONCEPTS[1].image} 
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/> */}
+            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem}/>))} //output list usando map 
+          </ul>
+        </section>
+    )
+}
