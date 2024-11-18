3800294d3692d9742abb0332287bbd6f121485ea
Karen <karen.silva@brisabr.com.br>
2024-11-18 15:41:30 -0300

    uso de ref

--- project-jsx-07/src/components/Player.jsx
+++ project-jsx-07/src/components/Player.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const input = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
 
-  function handleChange(event){
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
-  function handleClick(){
-    setSubmitted(true);
+  // function handleChange(event) {                                 PARA NÃO TER TANTO COD A GNT COLOCOU O USEREF E MUDDAMOS E TIRAMOA ALGUMAS COISAS
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
+  function handleClick() {
+    setEnteredPlayerName(input.current.value);
+    input.current.value="";
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+       <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2> {/* ?? É QUE NEM O enteredPlayerName ? enteredPlayerName : "unknown entity" , SÓ Q MAIS CURTO */}
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName}/>
+        <input
+          ref={input}
+          type="text"
+          // onChange={handleChange}
+          // value={enteredPlayerName}
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
