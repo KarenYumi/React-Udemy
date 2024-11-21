10e2eb8c5030316f9d392d06e460c2cfcc441577
Karen <karen.yumi20@gmail.com>
2024-11-21 16:12:54 -0300

    ajustando o pop-up

--- project-jsx-07/src/components/ResultModal.jsx
+++ project-jsx-07/src/components/ResultModal.jsx
@@ -1,11 +1,14 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime }, ref) {
   const dialog = useRef();
+  const userLost = remainingTime <= 0;
+  const formatandoRemainingTime = (remainingTime / 1000).toFixed(2);
+
   useImperativeHandle(ref, ()=> {
     return {
-      oper() {
-        dialog.current.ShowModal();
+      open() {
+        dialog.current.showModal();
 
       }
     };
@@ -13,12 +16,12 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime }, ref) 
 
   return (
     <dialog ref={dialog} className="result-modal" >
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formatandoRemainingTime} seconds left.</strong>
       </p>
       <form method="dialog">
         <button>Close</button>
@@ -26,4 +29,4 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime }, ref) 
     </dialog>
   );
 }) 
-export default ResultModal;
+export default ResultModal
