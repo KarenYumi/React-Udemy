32ae0b2ee11e7257db918d9da4b09bdd46f88d0f
Karen <karen.silva@brisabr.com.br>
2024-11-21 16:39:37 -0300

    fim do projeto

--- project-jsx-07/src/components/TimeChanllenge.jsx
+++ project-jsx-07/src/components/TimeChanllenge.jsx
@@ -5,18 +5,17 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timeRemaining, setTimeRemaining] = useState(targetTime *1000);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime *1000;
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-  if (timeRemaining <= 0){
+  if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
     dialog.current.open();
   }
 
-  function handleReset(){
-    
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
@@ -34,7 +33,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining}/>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         {/* {timerExpired && <p>You Lost</p>} */}
