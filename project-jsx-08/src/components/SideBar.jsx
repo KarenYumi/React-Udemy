f0a717fff93f0d4e745923605cab989b90bca21f
Karen <karen.silva@brisabr.com.br>
2024-11-22 13:41:26 -0300

    estilizando os blocos

--- project-jsx-08/src/components/SideBar.jsx
+++ project-jsx-08/src/components/SideBar.jsx
@@ -1,9 +1,9 @@
 export default function SideBar() {
   return (
-    <aside className="w-1/3 px-8 py-16 bg-stone-500 text-stone-50 md:w-72 rounded-r-xl">
-      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Seus Projetos</h2>
+    <aside className="ml-auto w-1/3 px-8 py-16 bg-stone-500 text-stone-50 md:w-96 rounded-l-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">ToDo List</h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-600 text-stone-300 hover:bg-stone-700 hover: text-stone-100">+ Novo Projeto</button>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-600 hover:bg-stone-700 hover: text-stone-100">+ Novo Item</button>
       </div>
       <ul></ul>
     </aside>
