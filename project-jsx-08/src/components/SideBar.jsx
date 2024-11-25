351d076c2319298fb1e73385629dd1619221199d
Karen <karen.yumi20@gmail.com>
2024-11-25 12:47:58 -0300

    Botão de delete e selecionar um item

--- project-jsx-08/src/components/SideBar.jsx
+++ project-jsx-08/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import Button from "./Button"
-export default function SideBar({ onStartAddProject, projects }) {
+export default function SideBar({ onStartAddProject, projects, onSelectedProject, selectedProjectId }) {
   return (
     <aside className="ml-auto w-1/3 px-8 py-16 bg-stone-500 text-stone-50 md:w-96 rounded-l-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">ToDo List</h2>
@@ -9,12 +9,26 @@ export default function SideBar({ onStartAddProject, projects }) {
         </Button>
       </div>
       <ul className="mt-7">
-        {projects.map(projects => <li key={projects.id}>
-          <button className="w-full text-left px-2 py-1 rounded-md my-1 text-stone-50 hover:font-bold hover:bg-stone-600">
-            {projects.title}
-          </button>
-        </li>)}
+        {projects.map((projects) => {
+          let cssClasses= "w-full text-left px-2 py-1 rounded-md my-1 text-stone-50 hover:font-bold hover:bg-stone-600";
+          if(projects.id === selectedProjectId){
+            cssClasses += " bg-stone-800 text-stone-200"
+          }else{
+            cssClasses += "text-stone-400"
+          }
+
+          return (
+            <li key={projects.id}>
+              <button
+                onClick={() => onSelectedProject(projects.id)}
+                className={cssClasses}
+              >
+                {projects.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
-  )
+  );
 }
