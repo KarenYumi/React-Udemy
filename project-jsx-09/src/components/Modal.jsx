05250faea04966586e4c7f88c3c4d59a76895b0d
Karen <karen.yumi20@gmail.com>
2024-11-28 15:19:07 -0300

    mini correção de erro

--- project-jsx-09/src/components/Modal.jsx
+++ project-jsx-09/src/components/Modal.jsx
@@ -1,13 +1,21 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
-  // dialog.current.showModal();
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal(); 
+    } else {
+      dialog.current.close();
+    }
+  }, [open]); // esse função irá funcionar de novo caso o open seja chamado
+
+
 
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById('modal')
