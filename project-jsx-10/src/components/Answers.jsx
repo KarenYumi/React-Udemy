960053c00ba23e80f4614e797ca1aa05e59ba11d
Karen <karen.silva@brisabr.com.br>
2024-12-02 14:32:36 -0300

    arrumei o bug de não mudar as respostas ao mudar a pergunta

---
+++ project-jsx-10/src/components/Answers.jsx
@@ -0,0 +1,38 @@
+
+import { useRef } from "react";
+export default function Answers({ answers, selectedAnswer , answerState , onSelect}) {
+  const shuffledAnswers = useRef();
+
+  if (!shuffledAnswers.current) {
+    //foi mudado de lugar pra ele funcionar apenas se ainda tiver questões
+    shuffledAnswers.current = [...answers]; //criamos uma cópia para poder deixar aleatorio a ordem
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = selectedAnswer === answer;
+        let cssClass = "";
+
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+
+        if ((answerState === "correct" || answerState === "wrong") && isSelected) {
+          cssClass = answerState;
+        }
+
+        return (
+          //key usado para destruir e reconstruir um componente
+          <li key={answer} className="answer" > 
+            <button onClick={() => onSelect(answer)} className={cssClass}>
+              {answer}
+            </button>
+          </li>
+        )
+      }
+      )}
+    </ul>
+  );
+}
