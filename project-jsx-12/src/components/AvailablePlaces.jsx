18716e3ab9d7002f5cee80a6229d560a439d3c68
Karen <karen.yumi20@gmail.com>
2024-12-05 15:46:35 -0300

    colocamos a posição dentro do try and catch

--- project-jsx-12/src/components/AvailablePlaces.jsx
+++ project-jsx-12/src/components/AvailablePlaces.jsx
@@ -1,28 +1,55 @@
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 
 import Places from './Places.jsx';
+import ErrorPage from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
-const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    async function fetchPlaces(){
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
+      } catch (error) {
+        setError(error);
+        setIsFetching(false);
+      }
     }
 
     fetchPlaces();
-  },[]);
+  }, []);
 
+
+  if (error) {
+    return <ErrorPage title="Aconteceu um erro" message={error.message} />
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
-      fallbackText="No places available."
+      isLoading={isFetching}
+      loadingText="Estamos preparando os lugares..."
+      fallbackText="Nenhum dado encontrad."
       onSelectPlace={onSelectPlace}
     />
   );
