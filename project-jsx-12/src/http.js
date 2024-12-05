d9c2cf9f3e5cdd61f1b39c4d9c54d870e8ebfbbb
Karen <karen.yumi20@gmail.com>
2024-12-05 16:26:18 -0300

    fazendo o update do item selecionado dentro do user-places

--- project-jsx-12/src/http.js
+++ project-jsx-12/src/http.js
@@ -9,3 +9,21 @@ export async function fetchAvailablePlaces() {
   return resData.places;
 };
 
+export async function updatedUserPlaces(places){
+  const response = await fetch("http://localhost:3000/user-places", {
+    method:"PUT",
+    body: JSON.stringify({places}),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const resData = await response.json();
+
+  if(!response.ok){
+    throw new Error("Falha no update do user");
+  }
+
+  return resData.message;
+}
+
