7e353d4b339ff93b7a12668fe9d41fd5a980a34a
Karen <karen.yumi20@gmail.com>
2024-12-09 15:51:47 -0300

    validating email

--- project-jsx-14/src/components/Sigup.jsx
+++ project-jsx-14/src/components/Sigup.jsx
@@ -7,6 +7,8 @@ export default function Signup() {
     const data = Object.fromEntries(fd.entries()); //da um array de todos os input fields
     data.acquisition = checkBox;
     console.log(data);
+
+    event.target.reset();
   }
 
 
--- project-jsx-14/src/components/StateLogin.jsx
+++ project-jsx-14/src/components/StateLogin.jsx
@@ -5,8 +5,15 @@ export default function Login() {
   // const [enteredPassword, setEnteredPassword]= useState("");
   const [enteredValues, setEnteredValues] = useState({
     email: "",
-    password: ""
+    password: "",
   });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,10 +22,21 @@ export default function Login() {
   }
 
   function handleInputChange(identifier, value) {
-    setEnteredValues(prevValues =>({
+    setEnteredValues((prevValues) => ({
       ...prevValues,
-      [identifier]: value
-    }))
+      [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true
+    }));
   }
 
   return (
@@ -28,20 +46,23 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" 
-          type="email" 
-          name="email" 
-          onChange={(event) => handleInputChange("email", event.target.value)} 
-          value={enteredValues.email} />
+          <input id="email"
+            type="email"
+            name="email"
+            onBlur={() => handleInputBlur("email")}
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
+          <div class="control-error">{emailIsInvalid && <p>Por favor colocar um email válido</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" 
-          type="password" 
-          name="password" 
-          onChange={(event) => handleInputChange("password", event.target.value)} 
-          value={enteredValues.password}
+          <input id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event.target.value)}
+            value={enteredValues.password}
           />
         </div>
       </div>
