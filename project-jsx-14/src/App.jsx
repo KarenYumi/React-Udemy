8904cee3ae521b6598dd36305f6200ff6027be92
Karen <karen.yumi20@gmail.com>
2024-12-09 16:37:10 -0300

    Colocando validações do file validation.js

--- project-jsx-14/src/App.jsx
+++ project-jsx-14/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './components/Header.jsx';
 import Login from './components/StateLogin.jsx';
-import Signup from './components/Sigup.jsx';
+// import Signup from './components/Sigup.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Signup />
+        <Login />
       </main>
     </>
   );
