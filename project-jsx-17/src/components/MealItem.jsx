10df0bc2106badda3d88a3328babe707b823dbc3
Karen <karen.yumi20@gmail.com>
2024-12-12 17:12:07 -0300

    CartContext e colocar o numero no botão do caart ao adicionar coisa

--- project-jsx-17/src/components/MealItem.jsx
+++ project-jsx-17/src/components/MealItem.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+
 import Button from "./UI/Button.jsx";
 import { currencyFormatter } from "./util/formatting.js";
+import CartContext from "../store/CartContext.jsx";
 
 export default function MealItem({meal}){
+  const cartCtx = useContext(CartContext);
+
   function handleAddMealToCart(){
-    
+    cartCtx.addItem(meal);
+
   }
 
   return(
@@ -16,7 +22,7 @@ export default function MealItem({meal}){
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
