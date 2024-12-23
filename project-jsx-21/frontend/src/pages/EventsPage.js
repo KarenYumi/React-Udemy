a14722f8f2b3bffd6608bd98fd480fd29681ead2
Karen <karen.yumi20@gmail.com>
2024-12-23 16:26:24 -0300

    fetching with loader(

--- project-jsx-21/frontend/src/pages/EventsPage.js
+++ project-jsx-21/frontend/src/pages/EventsPage.js
@@ -1,22 +1,13 @@
-import { Link } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
-const EVENTS = [
-  { id: 'event1', title: 'Event 1'},
-  { id: 'event2', title: 'Event 2'},
-  { id: 'event3', title: 'Event 3'},
-]
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
+  const events = useLoaderData();
+
   return (
     <>
-      <h1>The Events Page</h1>
-      <ul>
-        {EVENTS.map(event => (
-          <li key={event.id}>
-            <Link to={`/events/${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
