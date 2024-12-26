5224cbcad323c2ba9781ca382921215aa9c60217
Karen <karen.yumi20@gmail.com>
2024-12-26 14:08:11 -0300

    salvamos o loader dentro do eventsPage

--- project-jsx-21/frontend/src/App.js
+++ project-jsx-21/frontend/src/App.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
+import EventsPage, {loader as eventsLoader} from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailsPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
@@ -21,15 +21,7 @@ const router = createBrowserRouter([
         children: [
           {
             index: true, element: <EventsPage />, 
-            loader: async() => {
-              const response = await fetch('http://localhost:8080/events');
-              if (!response.ok) {
-
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
