3d3c9a0184711bb0a67ed1642e251dfdcfa4e4de
Karen <karen.yumi20@gmail.com>
2024-12-26 14:17:03 -0300

    navigation state

--- project-jsx-21/frontend/src/pages/Roots.js
+++ project-jsx-21/frontend/src/pages/Roots.js
@@ -1,11 +1,15 @@
+import { Outlet, useNavigation } from "react-router-dom";
+
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from "react-router-dom";
 
 function RootLayout() {
+  // const navigation = useNavigation(); //é um navigation state, no caso se for a uma pagina e demorar pra carregar, ele vai mostrar um loading
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
