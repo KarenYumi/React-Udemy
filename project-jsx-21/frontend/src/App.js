ccc8296ad933d886f7fb38e0dce0f7bb134fa46b
Karen <karen.yumi20@gmail.com>
2024-12-26 15:38:43 -0300

    useRouteLoaderData() acessa data from other routes

--- project-jsx-21/frontend/src/App.js
+++ project-jsx-21/frontend/src/App.js
@@ -26,11 +26,17 @@ const router = createBrowserRouter([
             loader: eventsLoader,
           },
           { path: ":eventId", 
-            element: <EventDetailPage />,
+            id: "event-detail",
             loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+              },
+              {path: "edit", element: <EditEventPage />},
+            ]
           },
           { path: "new", element: <NewEventPage /> },
-          { path: ":eventId/edit", element: <EditEventPage /> },
         ]
       }
 
--- project-jsx-21/frontend/src/pages/EditEventPage.js
+++ project-jsx-21/frontend/src/pages/EditEventPage.js
@@ -1,7 +1,10 @@
+import { useRouteLoaderData } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
+
 function EditEventPage() {
-  return <div>
-    <h1>Edit Event</h1>
-  </div>;
+  const data = useRouteLoaderData("event-detail");
+  return <EventForm event={data.event}/>;
 }
 
 export default EditEventPage;
