6563a2c5071b7a0fb5f089cb740590e160dbb069
Karen <karen.yumi20@gmail.com>
2025-01-02 17:04:55 -0300

    recebendo as info para editar

--- project-jsx-24/src/components/Events/EditEvent.jsx
+++ project-jsx-24/src/components/Events/EditEvent.jsx
@@ -1,20 +1,56 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvent } from '../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const params = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+
+  function handleSubmit(formData) { }
 
   function handleClose() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className='center'>
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="failed to laod event"
+          message={error.info?.message || " Por favor tente mais tarde"}
+        />
+        <div className='form-actions'>
+          <Link to="../" className='button'>
+            Okay
+          </Link>
+        </div>
+      </>
+    )
+  }
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +58,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
+    );
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      {content}
     </Modal>
   );
 }
