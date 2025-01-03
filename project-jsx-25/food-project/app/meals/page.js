2ad9a4c1ebe2d9e7a1bf81c4b1394c919adb64bf
Karen <karen.yumi20@gmail.com>
2025-01-03 16:23:14 -0300

    loading page

--- project-jsx-25/food-project/app/meals/page.js
+++ project-jsx-25/food-project/app/meals/page.js
@@ -1,30 +1,38 @@
 import Link from "next/link";
+import { Suspense } from "react";
+
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-export default async function MealsPage() {
-  const meals = await getMeals();
 
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+}
+
+export default async function MealsPage() {
 
   return (
     <>
       <header className={classes.header}>
         <h1>
           Delicious melas, crested{""}
-          <span className={classes.highlight}>by</span>
+          <span className={classes.highlight}> by you</span>
         </h1>
         <p>
           Choose your favorite rcipe and cool it ypurself at home!
         </p>
         <p className={classes.cta}>
           <Link href="/meals/share">
-          Share Your Favorite Recipe
+            Share Your Favorite Recipe
           </Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetchig meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
 
